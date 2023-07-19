c2ff43cd42b0024977d120c4b5f6a0914c535476
KevinB1500 <kdbautis@espol.edu.ec>
2023-07-19 09:11:17 -0500

    Modificaciones en base y todos los gets

--- Routes/getRoutes.js
+++ Routes/getRoutes.js
@@ -3,39 +3,87 @@ const router = express.Router();
 const sql = require('mssql');
 
 router.get('/getReportesNoAsignados', (req, res) => {
-    sql.query`SELECT id_reporte, descripcion, ubicacion, estado, estadoNuevo, created_at, latitud, longitud, prioridad
-        FROM Reporte
-        WHERE estadoNuevo = 'C'`
-    .then(result => {
-      res.json(result.recordset); // Enviamos los resultados en formato JSON
-    }).catch(err => {
-      console.error("Error al hacer consulta:", err);
-      res.status(500).send('Ocurrió un error al hacer la consulta a la base de datos');
-    });
+  sql.query`SELECT id_reporte, descripcion, ubicacion, estado, estado_nuevo, created_at, latitud, longitud, prioridad
+      FROM Reporte
+      WHERE estado_nuevo = 'C';`
+  .then(result => {
+    res.json(result.recordset); // Enviamos los resultados en formato JSON
+  }).catch(err => {
+    console.error("Error al hacer consulta:", err);
+    res.status(500).send('Ocurrió un error al hacer la consulta a la base de datos');
+  });
 });
 
 router.get('/getReportesAsignados', (req, res) => {
-    sql.query`SELECT id_reporte, descripcion, ubicacion, estado, estadoNuevo, created_at, latitud, longitud, prioridad
-        FROM Reporte
-        WHERE estadoNuevo != 'C' and estadoNuevo != 'F'`
-    .then(result => {
-      res.json(result.recordset);
-    }).catch(err => {
-      console.error("Error al hacer consulta:", err);
-      res.status(500).send('Ocurrió un error al hacer la consulta a la base de datos');
-    });
+  sql.query`SELECT id_reporte, descripcion, ubicacion, estado, estado_nuevo, created_at, latitud, longitud, prioridad
+      FROM Reporte
+      WHERE estado_nuevo != 'C' and estado_nuevo != 'F';`
+  .then(result => {
+    res.json(result.recordset);
+  }).catch(err => {
+    console.error("Error al hacer consulta:", err);
+    res.status(500).send('Ocurrió un error al hacer la consulta a la base de datos');
+  });
 });
 
 router.get('/getReportesFinalizados', (req, res) => {
-    sql.query`SELECT id_reporte, descripcion, ubicacion, estado, estadoNuevo, created_at, latitud, longitud, prioridad
-        FROM Reporte
-        WHERE estadoNuevo != 'F'`
-    .then(result => {
-      res.json(result.recordset);
-    }).catch(err => {
-      console.error("Error al hacer consulta:", err);
-      res.status(500).send('Ocurrió un error al hacer la consulta a la base de datos');
-    });
+  sql.query`SELECT id_reporte, descripcion, ubicacion, estado, estado_nuevo, created_at, latitud, longitud, prioridad
+      FROM Reporte
+      WHERE estado_nuevo != 'F';`
+  .then(result => {
+    res.json(result.recordset);
+  }).catch(err => {
+    console.error("Error al hacer consulta:", err);
+    res.status(500).send('Ocurrió un error al hacer la consulta a la base de datos');
+  });
+});
+
+router.get('/getLecturasPendientes', (req, res) => {
+  sql.query`SELECT	id_lectura, Lectura.id_medidor, Medidor.nombre, fecha_ultima_lectura, 
+      ultima_lectura, fecha_creacion, fecha_proxima_lectura, id_usuario_asignado,
+      Usuario.nombre, Usuario.apellido
+      FROM Lectura 
+      JOIN Medidor 
+      ON Lectura.id_medidor = Medidor.id_medidor
+      JOIN Usuario
+      ON Lectura.id_usuario_asignado = Usuario.id_usuario;
+      WHERE Lectura.estado != 'F';`
+  .then(result => {
+    res.json(result.recordset);
+  }).catch(err => {
+    console.error("Error al hacer consulta:", err);
+    res.status(500).send('Ocurrió un error al hacer la consulta a la base de datos');
+  });
+});
+
+router.get('/getLecturasFinalizadas', (req, res) => {
+  sql.query`SELECT	id_lectura, Lectura.id_medidor, Medidor.nombre, fecha_ultima_lectura, 
+      ultima_lectura, fecha_creacion, id_usuario_asignado, Usuario.nombre, Usuario.apellido
+      FROM Lectura 
+      JOIN Medidor 
+      ON Lectura.id_medidor = Medidor.id_medidor
+      JOIN Usuario
+      ON Lectura.id_usuario_asignado = Usuario.id_usuario
+      WHERE Lectura.estado = 'F';`
+  .then(result => {
+    res.json(result.recordset);
+  }).catch(err => {
+    console.error("Error al hacer consulta:", err);
+    res.status(500).send('Ocurrió un error al hacer la consulta a la base de datos');
+  });
+});
+
+router.get('/getUsuarios', (req, res) => {
+  sql.query`SELECT id_usuario, nombre, apellido, correo, tipo, Usuario.estado
+      FROM Usuario
+      JOIN Tipo_Usuario
+      ON Usuario.id_tipo_usuario = Tipo_Usuario.id_tipo_usuario;`
+  .then(result => {
+    res.json(result.recordset);
+  }).catch(err => {
+    console.error("Error al hacer consulta:", err);
+    res.status(500).send('Ocurrió un error al hacer la consulta a la base de datos');
+  });
 });
 
 module.exports = router;
--- Scripts/Agregar Columnas y Datos.sql
+++ Scripts/Agregar Columnas y Datos.sql
@@ -1,12 +1,50 @@
 -- Primero, añade la columna nueva
 ALTER TABLE Reporte
-ADD EstadoNuevo CHAR(1);
+ADD estado_nuevo CHAR(1);
 
 -- Luego, actualiza los valores en la columna nueva
 UPDATE Reporte
-SET estadoNuevo = 
+SET estado_nuevo = 
     CASE
         WHEN Estado = 'A' THEN 'C'
         WHEN Estado = 'I' THEN 'F'
-        ELSE estadoNuevo
+        ELSE estado_nuevo
     END;
+
+--Añadimos las columnas para medir el tiempo de todos los estados
+ALTER TABLE Reporte
+ADD fecha_aceptacion date,
+	hora_aceptacion time(7),
+	fecha_encurso date,
+	hora_encurso time(7);
+
+--Añadimos la tabla para registrar los medidores
+CREATE TABLE Medidor (
+  id_medidor INT PRIMARY KEY,
+  nombre VARCHAR(255),
+  latitud VARCHAR(50),
+  longitud VARCHAR(50)
+);
+
+--Añadimos la tabla para registrar las lecturas
+CREATE TABLE Lectura (
+  id_lectura INT PRIMARY KEY,
+  id_medidor INT,
+  id_usuario_asignado INT,
+  fecha_creacion DATE,
+  fecha_ultima_lectura DATE,
+  fecha_proxima_lectura DATE,
+  ultima_lectura DECIMAL(10, 2),
+  estado varchar(2),
+  FOREIGN KEY (id_medidor) REFERENCES Medidor(id_medidor),
+  FOREIGN KEY (id_usuario_asignado) REFERENCES Usuario(id_usuario)
+);
+
+--Añadimos la tabla para el histórico de lecturas
+CREATE TABLE Lecturas_Historicas (
+  id_lectura_historica INT PRIMARY KEY,
+  id_medidor INT,
+  fecha_lectura DATETIME,
+  lectura DECIMAL(10, 2),
+  FOREIGN KEY (id_medidor) REFERENCES Medidor(id_medidor)
+);
